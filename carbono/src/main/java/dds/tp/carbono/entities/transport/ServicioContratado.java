6f4f2d80b344fc6d2e24becf0703ff678ff6ec22
Fishu <ldfishkel@gmail.com>
2022-06-06 02:09:58 -0300

    tipo servicio contratado alta

--- carbono/src/main/java/dds/tp/carbono/entities/transport/ServicioContratado.java
+++ carbono/src/main/java/dds/tp/carbono/entities/transport/ServicioContratado.java
@@ -1,19 +1,13 @@
 package dds.tp.carbono.entities.transport;
 
 import dds.tp.carbono.entities.point.PuntoGeografico;
-import dds.tp.carbono.repository.admin.ServicioContratadoRepository;
 import dds.tp.carbono.services.distancia.CalculadorDistanciaServicioExterno;
 import lombok.Getter;
 import lombok.Setter;
 
 public class ServicioContratado implements MedioDeTransporte {
-    private ServicioContratadoRepository repository;
     @Getter @Setter private Integer id;
     @Getter @Setter private TipoServicioContratado tipo;
-
-    public ServicioContratado() {
-        this.repository = new ServicioContratadoRepository();
-    }
 
     @Override
     public Double calcularDistancia(PuntoGeografico inicio, PuntoGeografico fin) throws Exception {
