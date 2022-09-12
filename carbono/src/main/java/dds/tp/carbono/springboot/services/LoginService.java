64b5cb1d14a902b5dd2e7fc8669b26e23e1654d9
Juan Cruz Rey <juancurey2@gmail.com>
2022-09-11 23:24:22 -0300

    repofuchi

---
+++ carbono/src/main/java/dds/tp/carbono/springboot/services/LoginService.java
@@ -0,0 +1,23 @@
+package dds.tp.carbono.springboot.services;
+
+import dds.tp.carbono.entities.auth.Usuario;
+import dds.tp.carbono.http.utils.SessionCookie;
+import dds.tp.carbono.repository.auth.UsuarioRepository;
+
+public class LoginService {
+
+    private UsuarioRepository repository;
+
+    public LoginService() {
+        this.repository = new UsuarioRepository();
+    }
+
+    public SessionCookie login(String username, String password) throws Exception {
+        if (!this.repository.isValidLogin(username, password))
+            throw new Exception("Unauthorized");   
+        
+        Usuario usuario = this.repository.getUsuarioByUsername(username);
+        return new SessionCookie(usuario);
+    }
+}
+
